e639cf0b555788a1378920ed7b61f6b6875b2a57
titus <titus.gore@centria.fi>
2020-01-21 10:59:11 +0200

    exercise10 done

--- part1/printing/exercise_10/Program.cs
+++ part1/printing/exercise_10/Program.cs
@@ -7,7 +7,13 @@ namespace exercise_10
         public static void Main(string[] args)
         {
             // Write your code here:
+        Console.WriteLine("I would like you to give your first name and profession and i will predict your future");
+        Console.WriteLine("Give your first name");
+        string message = Console.ReadLine();
+        Console.WriteLine("Give your profession");
+        string greeting = Console.ReadLine();
+        Console.WriteLine("based on your profession you should know that time travel is impossible hence your future cannot be fortold!");
 
-        }
+      }
     }
 }

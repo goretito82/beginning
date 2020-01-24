94ab253c95c52b7ebf571e07a83488132d32e474
titus <titus.gore@centria.fi>
2020-01-24 23:43:25 +0200

    exercise 17 done

--- part1/calculations/exercise_17/Program.cs
+++ part1/calculations/exercise_17/Program.cs
@@ -7,8 +7,30 @@ namespace exercise_17
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+          
 
-    }
+            Console.Write("Give First integer value: ");
+
+            // Declare and assign the input as string
+            string userInput = Console.ReadLine();
+
+            // Convert string to integer type
+            int intValue = Convert.ToInt32(userInput);
+
+            Console.WriteLine("First Number " + intValue);
+
+            Console.Write("Give double value: ");
+
+            // Assign new value to userInput
+            userInput = Console.ReadLine();
+
+            // Convert string to double type
+            double doubleValue = Convert.ToDouble(userInput);
+
+            Console.WriteLine("Second Number " + doubleValue);
+            Console.WriteLine(" Your sum is ");
+            Console.Write(intValue + doubleValue);
+
+        }
   }
 }

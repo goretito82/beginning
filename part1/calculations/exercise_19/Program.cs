91ca603c0a8dc1cdc09793237917fad77c65b81d
titus <titus.gore@centria.fi>
2020-01-25 08:47:14 +0200

    First commit

--- part1/calculations/exercise_19/Program.cs
+++ part1/calculations/exercise_19/Program.cs
@@ -6,9 +6,31 @@ namespace exercise_19
   {
     public static void Main(string[] args)
     {
+            
 
-      // Write your code here:
+            // declare first int
+            Console.WriteLine("First Integer ");
+            // Declare and assign the input as string
+            string userInput = Console.ReadLine();
+            // Convert string to integer type
+            int intvalue = Convert.ToInt32(userInput);
 
+            
+
+            // declare second int
+            Console.WriteLine("Second Integer ");
+            // Declare and assign the input as string
+            string userInput2 = Console.ReadLine();
+            // Convert string to integer type
+            int intValue2 = Convert.ToInt32(userInput2);
+
+            
+
+            
+            Console.WriteLine("sum is " + userInput + "+" + userInput2);
+
+            Console.Write(intvalue + intValue2);
+           
+        }
     }
-  }
 }

c41670331856065899c6d18ce2b6adec77f7a502
titus <titus.gore@centria.fi>
2020-01-26 12:52:26 +0200

    exercise 33 done

--- part1/conditionals/exercise_32/Program.cs
+++ part1/conditionals/exercise_32/Program.cs
@@ -4,10 +4,38 @@ namespace exercise_32
 {
   class Program
   {
-    public static void Main(string[] args)
-    {
-      // Write your code here:
+     
 
-    }
+        public static void Main(string[] args)
+    {
+
+          
+          
+
+            // declare string
+            Console.WriteLine("Speak, friend and enter");
+            string userinput = Console.ReadLine();
+           
+            
+         
+
+            bool mellon = true;
+            
+            if (mellon)
+
+            {
+                Console.WriteLine("welcome, friend ");
+            }
+            
+
+            else 
+
+            {
+                Console.WriteLine("They've got a cave troll!");
+            }
+
+
+
+     }
   }
 }

bc6b68eb7c5982849103a4bec2d7eb3dd67c0fc5
titus <titus.gore@centria.fi>
2020-01-28 19:08:45 +0200

    done

--- part2/subproblems/exercise_44/Program.cs
+++ part2/subproblems/exercise_44/Program.cs
@@ -7,8 +7,49 @@ namespace exercise_44
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+            while (true)
+            {
+                //input integer convert it
+                Console.WriteLine(" Please put in frst integer, 0 exits ");
+                int intvalue = Convert.ToInt32(Console.ReadLine());
 
-    }
+                //input 2nd integer
+                Console.WriteLine(" Please put in second integer, 0 exits ");
+                int intvalue2 = Convert.ToInt32(Console.ReadLine());
+
+
+                //halt prog
+                if (intvalue == 0)
+                {
+                    break;
+
+                }
+
+                if (intvalue > intvalue2)
+                {
+
+                Console.WriteLine( intvalue + " is greater than " + intvalue2 );
+                    continue;
+                }
+                if (intvalue < intvalue2)
+                {
+                   
+                    Console.WriteLine(intvalue + " is lesser than " + intvalue2);
+                    continue;
+                }
+
+                if (intvalue == intvalue2)
+                {
+
+                    Console.WriteLine(intvalue + " is equal to " + intvalue2);
+                    continue;
+                }
+
+
+
+            }
+
+
+        }
   }
 }

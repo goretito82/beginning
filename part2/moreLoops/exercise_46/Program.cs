88c31540f30cad5f10d7b16006e92a000c5ca4a5
titus <titus.gore@centria.fi>
2020-01-29 13:32:34 +0200

    done

--- part2/moreLoops/exercise_46/Program.cs
+++ part2/moreLoops/exercise_46/Program.cs
@@ -6,9 +6,19 @@ namespace exercise_46
   {
     public static void Main(string[] args)
     {
+            
+            int number = 0;
 
-      // Write your code here:
+            Console.WriteLine("Please input integer less than 100");
+            int input = Convert.ToInt32(Console.ReadLine());
 
+            while (input < 100)
+            {
+             Console.WriteLine(number + input +1);
+             input++;
+
+            }
+
+        }
     }
-  }
 }

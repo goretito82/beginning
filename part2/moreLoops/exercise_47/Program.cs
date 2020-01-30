c71061eabff1104ad900d3b037e309119822feb0
titus <titus.gore@centria.fi>
2020-01-30 11:39:16 +0200

    done

--- part2/moreLoops/exercise_47/Program.cs
+++ part2/moreLoops/exercise_47/Program.cs
@@ -2,13 +2,39 @@
 
 namespace exercise_47
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
+        public static void Main(string[] args)
+        {
+            //int start = 1;
+            int number = 0;
 
-      // Write your code here:
-      
+
+            while (true)
+            {
+                Console.WriteLine("Please where to stop");
+                int input = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("please input where to begin");
+                int input2 = Convert.ToInt32(Console.ReadLine());
+
+
+                if (input == 0)
+                {
+                    break;
+                }
+                
+                while (input2 < input)
+                {
+                    Console.WriteLine(number + input2 + 1);
+                    input2++;
+                }
+
+
+            }
+
+
+        }
     }
-  }
 }
+

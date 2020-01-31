99ec0f3f8cc2ad096c23860dd92fc0ce0c654788
titus <titus.gore@centria.fi>
2020-01-31 16:06:14 +0200

    done

--- part2/methods/exercise_50/Program.cs
+++ part2/methods/exercise_50/Program.cs
@@ -7,24 +7,28 @@ namespace exercise_50
         public static void Main(string[] args)
         {
 
-            int end = 1;
-            for (int i=0; i < end; i++)
+        
 
-            Console.Write(" enter number ");
+                Console.Write(" enter number ");
             int number = Convert.ToInt32(Console.ReadLine());
 
-           
-                while (true)           
+            int i = 0;
+            while (i < number)
+            {
+                Console.WriteLine();
+                i++;
+
                 Printphrase();
-            
+            }
 
         }
 
         public static void Printphrase()
-               
+
         {
             Console.WriteLine("In a hole in the ground there lived a method!");
         }
+
     }
 
  }

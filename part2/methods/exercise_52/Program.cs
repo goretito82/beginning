09d07c795c0d472f46783739a44888915a922400
titus <titus.gore@centria.fi>
2020-02-01 21:27:54 +0200

    done

--- part2/methods/exercise_52/Program.cs
+++ part2/methods/exercise_52/Program.cs
@@ -2,15 +2,27 @@
 
 namespace exercise_52
 {
-  class Program
-  {
-    public static void Main(String[] args)
+    class Program
     {
-      // Call your method here:
+        public static void Main(String[] args)
+        {
+            // Call your method here:
 
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            PrintFromNumberToOne(number1);
+
+
+        }
+
+        // Write your method here:
+        public static void PrintFromNumberToOne(int numero)
+        {
+            while (numero-- > 0)
+               
+            {
+                Console.WriteLine(numero);
+            }
+
+        }
     }
-
-    // Write your method here:
-
-  }
 }

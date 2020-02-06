19dfca99153d475b04d2f915dd7c45b1b1981432
titus <titus.gore@centria.fi>
2020-02-06 13:56:52 +0200

    done

--- part2/methods/exercise_57/Program.cs
+++ part2/methods/exercise_57/Program.cs
@@ -6,13 +6,16 @@ namespace exercise_57
   {
     public static int Sum(int number1, int number2, int number3, int number4)
     {
-      // write your code here
-      // remember to include return (at the end)!
-    }
+            // write your code here
+            int sum = number1 + number2 + number3 + number4;
+
+            return sum;
+            // remember to include return (at the end)!
+        }
 
     public static void Main(String[] args)
     {
-      int answer = Sum(4, 3, 6, 1);
+      int answer = Sum(444, 344, 666, 111);
       Console.WriteLine("Sum: " + answer);
     }
   }

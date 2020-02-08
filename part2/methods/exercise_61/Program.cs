df442e3cf9b41e838eba976c6b4a11216f643e59
titus <titus.gore@centria.fi>
2020-02-08 16:19:50 +0200

    done

--- part2/methods/exercise_61/Program.cs
+++ part2/methods/exercise_61/Program.cs
@@ -2,12 +2,61 @@
 
 namespace exercise_61
 {
-  class Program
-  {
-    public static void Main(String[] args)
+    class Program
     {
 
+        public static void PrintStars(int number)
+        {
+            int i = 0;
+            while (i < number)
+            {
+                Console.Write("*");
+                i++;
+            }
+             Console.WriteLine("");
+        }
+        public static void ChristmasTree(int height)
+        {
+
+            for (int n = 1; n < height + 3; n++)
+                
+             Console.WriteLine(n <= height ?
+
+            new String('*', n * 2 - 1).PadLeft(height + n) :
+
+             "*".PadLeft(height + 2));
+            
+        }
+        public static void Main(String[] args)
+        {
+            PrintStars(2);
+            PrintRightTriangle(4);
+            ChristmasTree(10);
+            ChristmasTree(4);
+
+        }
+        public static void PrintSpaces(int number)
+        {
+            int i = 0;
+            while (i < number)
+            {
+            Console.Write(" ");
+                i++;
+            }
+            //Console.WriteLine("");
+        }
+        public static void PrintRightTriangle(int size)
+
+        {
+            for (int i=1; i<=size; i++)
+            {
+                PrintSpaces(size - i);
+                PrintStars(i);
+            }
+
+        }
+      }
+        
     }
 
-  }
-}
+

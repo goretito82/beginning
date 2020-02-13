5a34779edb635a8fedda39703e702c9d40335f17
titus <titus.gore@centria.fi>
2020-02-13 17:24:21 +0200

    done

--- part3/lists/exercise_70/Program.cs
+++ part3/lists/exercise_70/Program.cs
@@ -3,20 +3,35 @@ using System.Collections.Generic;
 
 namespace exercise_70
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    internal class Program
     {
-      List<int> list = new List<int>();
-      while (true)
-      {
-        int input = Convert.ToInt32(Console.ReadLine());
-        if (input == -1)
+        public static void Main(string[] args)
         {
-          break;
+            List<int> list = new List<int>();
+            while (true)
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+                if (input == -1)
+                {
+                    break;
+                }
+                list.Add(input);
+
+                // assume we have a list that contains integers
+
+                int greatest = list[0];
+
+                for (int i = 0; i < list.Count; i++)
+                {
+                    int number = list[i];
+                    if (greatest < number)
+                    {
+                        greatest = number;
+                    }
+                }
+
+                Console.WriteLine("The Greatest number: " + greatest);
+            }
         }
-        list.Add(input);
-      }
     }
-  }
 }
--- part3/lists/exercise_70/ProgramTests.cs
+++ part3/lists/exercise_70/ProgramTests.cs
@@ -1,25 +1,24 @@
-using System;
-using System.IO;
 using exercise_70;
 using NUnit.Framework;
-using System.Text.RegularExpressions;
+using System;
+using System.IO;
 
 namespace ProgramTests
 {
-  [TestFixture]
-  public class TestProgram
-  {
-    [Test]
-    public void TestExercise70Once()
+    [TestFixture]
+    public class TestProgram
     {
-      using (StringWriter sw = new StringWriter())
-      {
-        TextWriter stdout = Console.Out;
-
-        Console.SetOut(sw);
-
-        var data = String.Join(Environment.NewLine, new[]
+        [Test]
+        public void TestExercise70Once()
         {
+            using (StringWriter sw = new StringWriter())
+            {
+                TextWriter stdout = Console.Out;
+
+                Console.SetOut(sw);
+
+                var data = String.Join(Environment.NewLine, new[]
+                {
                 "12",
                 "345",
                 "123",
@@ -27,28 +26,28 @@ namespace ProgramTests
                 "-1"
                 });
 
-        Console.SetIn(new System.IO.StringReader(data));
+                Console.SetIn(new System.IO.StringReader(data));
 
-        Program.Main(null);
+                Program.Main(null);
 
-        Console.SetOut(stdout);
+                Console.SetOut(stdout);
 
-        // Assert
-        Assert.AreEqual("The greatest number: 345\n", sw.ToString().Replace("\r\n", "\n"), "Remember to find the greatest!!");
-      }
-    }
+                // Assert
+                Assert.AreEqual("The greatest number: 345\n", sw.ToString().Replace("\r\n", "\n"), "Remember to find the greatest!!");
+            }
+        }
 
-    [Test]
-    public void TestExercise70Twice()
-    {
-      using (StringWriter sw = new StringWriter())
-      {
-        TextWriter stdout = Console.Out;
-
-        Console.SetOut(sw);
-
-        var data = String.Join(Environment.NewLine, new[]
+        [Test]
+        public void TestExercise70Twice()
         {
+            using (StringWriter sw = new StringWriter())
+            {
+                TextWriter stdout = Console.Out;
+
+                Console.SetOut(sw);
+
+                var data = String.Join(Environment.NewLine, new[]
+                {
                 "12",
                 "11",
                 "10",
@@ -61,41 +60,41 @@ namespace ProgramTests
                 "-1"
                 });
 
-        Console.SetIn(new System.IO.StringReader(data));
+                Console.SetIn(new System.IO.StringReader(data));
 
-        Program.Main(null);
+                Program.Main(null);
 
-        Console.SetOut(stdout);
+                Console.SetOut(stdout);
 
-        // Assert
-        StringAssert.Contains("The greatest number: 42\n", sw.ToString().Replace("\r\n", "\n"), "Remember to check which is the greatest!");
-      }
-    }
+                // Assert
+                StringAssert.Contains("The greatest number: 42\n", sw.ToString().Replace("\r\n", "\n"), "Remember to check which is the greatest!");
+            }
+        }
 
-    [Test]
-    public void TestExercise70Single()
-    {
-      using (StringWriter sw = new StringWriter())
-      {
-        TextWriter stdout = Console.Out;
-
-        Console.SetOut(sw);
-
-        var data = String.Join(Environment.NewLine, new[]
+        [Test]
+        public void TestExercise70Single()
         {
+            using (StringWriter sw = new StringWriter())
+            {
+                TextWriter stdout = Console.Out;
+
+                Console.SetOut(sw);
+
+                var data = String.Join(Environment.NewLine, new[]
+                {
                 "2",
                 "-1"
                 });
 
-        Console.SetIn(new System.IO.StringReader(data));
+                Console.SetIn(new System.IO.StringReader(data));
 
-        Program.Main(null);
+                Program.Main(null);
 
-        Console.SetOut(stdout);
+                Console.SetOut(stdout);
 
-        // Assert
-        StringAssert.Contains("The greatest number: 2\n", sw.ToString().Replace("\r\n", "\n"), "Only one number, should be easy!");
-      }
+                // Assert
+                StringAssert.Contains("The greatest number: 2\n", sw.ToString().Replace("\r\n", "\n"), "Only one number, should be easy!");
+            }
+        }
     }
-  }
 }

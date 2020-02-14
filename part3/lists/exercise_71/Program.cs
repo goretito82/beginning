c426f5f3179085ba34f46d20da1394e443f5072e
titus <titus.gore@centria.fi>
2020-02-14 05:17:39 +0200

    done

--- part3/lists/exercise_71/Program.cs
+++ part3/lists/exercise_71/Program.cs
@@ -5,19 +5,25 @@ namespace exercise_71
 {
   class Program
   {
-    public static void Main(string[] args)
-    {
-      List<int> list = new List<int>();
-      while (true)
-      {
-        int input = Convert.ToInt32(Console.ReadLine());
-        if (input == -1)
+        public static void Main(string[] args)
         {
-          break;
-        }
-        list.Add(input);
-      }
+            List<int> list = new List<int>();
+            while (true)
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+                if (input == -1)
+                {
+                    break;
+                }
+                list.Add(input);
+            }
+            Console.WriteLine("Search for");
+            int search = Convert.ToInt32(Console.ReadLine());
 
-    }
+           
+            int index = list.IndexOf(search);
+
+            Console.WriteLine("is at index " + index);
+        }
   }
 }

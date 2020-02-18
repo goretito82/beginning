3b6a6b6b7a318b57b0fc8159f13178e7791489e6
titus <titus.gore@centria.fi>
2020-02-18 12:52:02 +0200

    done

--- part3/lists/exercise_75/Program.cs
+++ part3/lists/exercise_75/Program.cs
@@ -1,26 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace exercise_75
 {
   class Program
   {
-    public static void Main(string[] args)
+   public static void PrintNumbersInRange(List<int> numbers, int lowerLimit, int upperLimit)
+
+   {
+   foreach (int number in numbers.Where (number => number >= lowerLimit && number <= upperLimit))
     {
-
-
-      // Example method calls for testing your method.
-      // List<int> numbers = new List<int>();
-      // numbers.Add(5);
-
-      Console.WriteLine("The numbers in the range [0, 5]");
-      PrintNumbersInRange(numbers, 0, 5);
-
-      Console.WriteLine("The numbers in the range [3, 10]");
-      PrintNumbersInRange(numbers, 3, 10);
+    Console.WriteLine(number);
     }
+   }
+            public static void Main(string[] args)
+   {
+                // Example method calls for testing your method.
+          List<int> numbers = new List<int>();
+          numbers.Add(3);
+          numbers.Add(2);
+          numbers.Add(6);
+          numbers.Add(-1);
+          numbers.Add(5);
+          numbers.Add(1);
 
+    Console.WriteLine("The numbers in the range [0, 5]");
+    PrintNumbersInRange(numbers, 0, 5);
+    }
     }
   }
-}
-
+    

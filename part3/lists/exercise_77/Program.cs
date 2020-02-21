8c30f64df1b169eefb433e121a63e84bcdfa22c2
Titus Gore SOFTK20P <titus.gore@centria.fi>
2020-02-21 11:21:19 +0200

    done77

--- part3/lists/exercise_77/Program.cs
+++ part3/lists/exercise_77/Program.cs
@@ -7,20 +7,26 @@ namespace exercise_77
   {
     public static void Main(string[] args)
     {
-      // You can try your method here if you want
+      
+    //You can try your method here if you want
+     
       List<string> strings = new List<string>();
           
-        RemoveLast(strings);
-             
-    }
-    public static void RemoveLast(List<string> strings)
-    {
+        //if (strings.Count > 0)  
+      strings.Add ("First");
+      strings.Add ("second");
+      strings.Add ("Third");
+       
+    
+      RemoveLast(strings);
+      strings.ForEach(Console.WriteLine);
       
-       strings.Add("First");
-       strings.Add("second");
-       strings.Add("Third");
-
-       strings.ForEach(Console.WriteLine);
+     }
+    public static void RemoveLast(List<string> strings)
+   
+    {
+        strings.RemoveAt(strings.Count - 1); 
+       
 
     }
   }

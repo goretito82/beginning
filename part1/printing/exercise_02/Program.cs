b693cfc6b4582e8ad0a828196f9a961e4ebb0a8a
titus <titus.gore@centria.fi>
2020-02-22 10:53:23 +0200

    done2

--- part1/printing/exercise_02/Program.cs
+++ part1/printing/exercise_02/Program.cs
@@ -9,7 +9,7 @@ namespace exercise_02
             // Your code here:
         Console.WriteLine("Once upon a time");
         Console.WriteLine("I was falling in love");
-        Console.WriteLine("Now I'm falling apart");
+        Console.WriteLine("Now I'm only falling apart");
 
 
         }

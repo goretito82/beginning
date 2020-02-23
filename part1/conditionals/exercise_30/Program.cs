5dacfb0fe96e7b97bc0036303f0a95e2b321c90b
titus <titus.gore@centria.fi>
2020-02-23 19:25:24 +0200

    done30

--- part1/conditionals/exercise_30/Program.cs
+++ part1/conditionals/exercise_30/Program.cs
@@ -7,51 +7,51 @@ namespace exercise_30
     public static void Main(string[] args)
     {
             // declare first integer
-            Console.WriteLine("Give your percentage");
+            Console.WriteLine("Give your percent [0 - 100]:");
             string userinput = Console.ReadLine();
             int intvalue = Convert.ToInt32(userinput);
 
 
 
-            if (intvalue == 0)
+            if (intvalue < 0)
             {
-                Console.WriteLine("impossible " + userinput);
+                Console.WriteLine("Impossible");
 
             }
             else if (intvalue < 50)
             {
-                Console.WriteLine("Fail " + userinput);
+                Console.WriteLine("Fail");
             }
 
             else if (intvalue >= 50 && intvalue <= 59)
 
             {
-                Console.WriteLine("Grade 1 " + userinput);
+                Console.WriteLine("Grade: 1");
             }
 
             else if (intvalue >= 60 && intvalue <= 69)
             {
-                Console.WriteLine("Grade 2 " + userinput);
+                Console.WriteLine("Grade: 2");
 
             }
             else if (intvalue >= 70 && intvalue <= 79)
             {
-                Console.WriteLine("Grade 3 " + userinput);
+                Console.WriteLine("Grade: 3");
 
             }
             else if (intvalue >= 80 && intvalue <= 89)
             {
-                Console.WriteLine("Grade 4 " + userinput);
+                Console.WriteLine("Grade: 4");
 
             }
             else if (intvalue >= 90 && intvalue <= 100)
             {
-                Console.WriteLine("Grade 5 " + userinput);
+                Console.WriteLine("Grade: 5");
 
             }
             else if  (intvalue > 100)
             {
-                Console.WriteLine("Outstanding! " + userinput);
+                Console.WriteLine("Outstanding!");
             }
         }
   }

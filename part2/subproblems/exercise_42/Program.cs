3f0c30710702f23f64deedce5c80b95dd9f7c5a4
titus <titus.gore@centria.fi>
2020-02-23 21:20:05 +0200

    done_42

--- part2/subproblems/exercise_42/Program.cs
+++ part2/subproblems/exercise_42/Program.cs
@@ -6,48 +6,31 @@ namespace exercise_42
     {
         public static void Main(string[] args)
         {
-           // int sum = 0;
-          //  int total = 0;
-
+         
             while (true)
             {
                 //input integer convert it
-                Console.WriteLine(" Please put in an integer, 0 exits ");
-
+              
                 int intvalue = Convert.ToInt32(Console.ReadLine());
-
-                //input second integer
-                Console.WriteLine(" Please put in an integer, 0 exits ");
-
+              //input second integer
                 int intvalue2 = Convert.ToInt32(Console.ReadLine());
-
-
 
 
                 if (intvalue == 0)
                 {
                     break;
-
                 }
                 if (intvalue2 == 0)
                 {
                     break;
-
                 }
-
 
                 if (intvalue > 1)
                 {
-
-                    int number = intvalue + intvalue2;
-                    double squareroot = Math.Sqrt(number);
-                    Console.WriteLine("the square root of the total sum is " + squareroot);
-
-
+                 int number = intvalue + intvalue2;
+                 double squareroot = Math.Sqrt(number);
+                    Console.WriteLine(squareroot);
                 }
-
-
-
             }
         }
     }

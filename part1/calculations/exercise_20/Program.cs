b69d5bbdfd50ef81ad53f70f2cb9465f49765644
titus <titus.gore@centria.fi>
2020-02-23 04:47:39 +0200

    done_20

--- part1/calculations/exercise_20/Program.cs
+++ part1/calculations/exercise_20/Program.cs
@@ -7,20 +7,20 @@ namespace exercise_20
     public static void Main(string[] args)
     {
             //decalre first integer
-            Console.WriteLine("Give the first number");
+            Console.WriteLine("Give the first number!");
 
             string userinput = Console.ReadLine();
             int intvalue = Convert.ToInt32(userinput);
 
             //second integer
-            Console.WriteLine("Give the second number");
+            Console.WriteLine("Give the second number!");
 
             string userinput2 = Console.ReadLine();
             int intvalue2 = Convert.ToInt32(userinput2);
 
-            Console.WriteLine("Answer is " + userinput + "*" + userinput2);
+            Console.WriteLine( userinput + " * " + userinput2 );
             
-            Console.Write(intvalue * intvalue2);
+            Console.Write( intvalue * intvalue2 );
 
 
 

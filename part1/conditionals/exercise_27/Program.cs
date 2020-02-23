f654d887293d39c7012a43e036f39fea061fe4fe
titus <titus.gore@centria.fi>
2020-02-23 11:03:03 +0200

    done27

--- part1/conditionals/exercise_27/Program.cs
+++ part1/conditionals/exercise_27/Program.cs
@@ -7,17 +7,17 @@ namespace exercise_27
     public static void Main(string[] args)
     {
             //declare number
-            Console.WriteLine("Give a number");
+            Console.WriteLine("Give a number:");
             string userinput = Console.ReadLine();
             int intvalue = Convert.ToInt32(userinput);
 
             if (intvalue > 0)
             {
-                Console.WriteLine("it is positive");
+                Console.WriteLine("It is positive");
             }
             else
             {
-                Console.WriteLine("It is not positive ");
+                Console.WriteLine("It is not positive");
             }
         }
   }

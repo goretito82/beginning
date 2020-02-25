556f03640499c2ac14d87a8810a51e0f3901d691
titus <titus.gore@centria.fi>
2020-02-25 08:58:55 +0200

    done12

--- part1/variables/exercise_12/Program.cs
+++ part1/variables/exercise_12/Program.cs
@@ -6,7 +6,7 @@ namespace exercise_12
   {
     public static void Main(string[] args)
     {
-   Console.Write("Give a number! ");
+   Console.WriteLine("Give a number!");
 
 // Declare and assign the input as string
 string userInput = Console.ReadLine();

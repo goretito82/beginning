705a854280a4fbae020f6a87de1bc35dffaf8700
titus <titus.gore@centria.fi>
2020-02-26 10:14:15 +0200

    done66

--- part3/lists/exercise_66/Program.cs
+++ part3/lists/exercise_66/Program.cs
@@ -1,29 +1,4 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-
-namespace exercise_66
-{
-  class Program
-  {
-    public static void Main(string[] args)
-    {
-      List<string> list = new List<string>();
-      while (true)
-      {
-        string input = Console.ReadLine();
-        if (input == "")
-        {
-          break;
-        }
-        list.Add(input);
-      }
-
-    }
-  }
-}
-=======
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 
 namespace exercise_66
@@ -47,4 +22,3 @@ namespace exercise_66
     }
   }
 }
->>>>>>> 88973343e8c79c90bd5caa4532095132873f9374

191910e9a57f4073d1b4531c535862ac64d6e9d8
titus <titus.gore@centria.fi>
2020-02-26 19:16:38 +0200

    done72

--- part3/lists/exercise_72/Program.cs
+++ part3/lists/exercise_72/Program.cs
@@ -1,28 +1,13 @@
-<<<<<<< HEAD
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace exercise_72
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-
-    }
-  }
-}
-=======
-﻿using System;
-using System.Collections.Generic;
-
-namespace exercise_72
-{
-  class Program
-  {
-    public static void Main(string[] args)
-    {
- List<int> list = new List<int>();
+        public static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
             while (true)
             {
                 int input = Convert.ToInt32(Console.ReadLine());
@@ -30,28 +15,31 @@ namespace exercise_72
                 {
                     break;
                 }
-               list.Add(input);
+                list.Add(input);
+            }
+            // assume we have a list that contains integers
 
-                // assume we have a list that contains integers
-                   
-                   int smallest = list[0];
-                   
-
-                    for (int i = 0; i < list.Count; i++)
+            int smallest = list[0];
+            for (int i = 0; i < list.Count; i++)
+            {
+                int number = list[i];
+                if (smallest > number)
                 {
-                    int number = list[i];
-                    if (smallest > number)
-                    {
-                        smallest = number;
-                    } 
-                    
-                 int index = list.IndexOf(number);  
-                Console.WriteLine("The smallest number: " + smallest);
-                Console.WriteLine("Found at index " + index);
-                 
+                    smallest = number;
                 }
-             }
+            }
+            Console.WriteLine("Smallest number: " + smallest);
+            
+            for (int index = 0; index < list.Count; index++)
+            {
+                
+                if (list[index] == smallest)
+                
+
+                {
+                    Console.WriteLine("Found at index: " + index);
+                }
+            }
         }
-     }
-}  
->>>>>>> 88973343e8c79c90bd5caa4532095132873f9374
+    }
+}

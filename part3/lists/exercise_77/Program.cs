c2c2e08fb5e627ada76c46a5751ead3c1e9ce9e4
titus <titus.gore@centria.fi>
2020-02-26 21:34:08 +0200

    done77

--- part3/lists/exercise_77/Program.cs
+++ part3/lists/exercise_77/Program.cs
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-
-namespace exercise_77
-{
-  class Program
-  {
-    public static void Main(string[] args)
-    {
-      
-    //You can try your method here if you want
-     
-      List<string> strings = new List<string>();
-          
-        //if (strings.Count > 0)  
-      strings.Add ("First");
-      strings.Add ("second");
-      strings.Add ("Third");
-       
-    
-      RemoveLast(strings);
-      strings.ForEach(Console.WriteLine);
-      
-     }
-    public static void RemoveLast(List<string> strings)
-   
-    {
-        strings.RemoveAt(strings.Count - 1); 
-       
-
-    }
-  }
-}
-
-
-=======
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -45,28 +8,20 @@ namespace exercise_77
     public static void Main(string[] args)
     {
      // You can try your method here if you want
+       List<string> strings = new List<string>();
+       strings.Add("First");
+       strings.Add("Second");
+       strings.Add("Third");      
+          
+        RemoveLast(strings);
+      strings.ForEach(Console.WriteLine);
       
-
-        foreach (string element in strings)
-            Console.WriteLine(element);
-   
     }
     
    public static void RemoveLast(List<string> strings)
      {   
-         
-         List<string> strings = new List<string>();
-        
-     strings.Add("First");
-     strings.Add("Second");
-     strings.Add("Third");
-     strings.ForEach(Console.WriteLine);
+      strings.RemoveAt(strings.Count - 1); 
+
+   }
     }
-}
-
-<<<<<<< HEAD
-}
-=======
-
->>>>>>> 88973343e8c79c90bd5caa4532095132873f9374
->>>>>>> 74707bace3480c371c12e87d8b1d564e75b4cb89
+ }

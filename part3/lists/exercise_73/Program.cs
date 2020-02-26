b54faebb2a0fdd19dd84f99bdb07efd8ccca8918
titus <titus.gore@centria.fi>
2020-02-26 20:29:10 +0200

    done73

--- part3/lists/exercise_73/Program.cs
+++ part3/lists/exercise_73/Program.cs
@@ -1,55 +1,30 @@
-<<<<<<< HEAD
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace exercise_73
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      List<int> list = new List<int>();
-      while (true)
-      {
-        int input = Convert.ToInt32(Console.ReadLine());
-        if (input == -1)
+        public static void Main(string[] args)
         {
-          break;
-        }
-        list.Add(input);
-      }
+            List<int> list = new List<int>();
+            //int sum = 0;
+            while (true)
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+                if (input == -1)
+                {
+                    break;
+                }
+                list.Add(input);
 
-    }
-  }
-}
-=======
-﻿using System;
-using System.Collections.Generic;
-
-namespace exercise_73
-{
-  class Program
-  {
-    public static void Main(string[] args)
-    {
-      List<int> list = new List<int>();
-      int sum = 0;
-      while (true)
-      {
-        int input = Convert.ToInt32(Console.ReadLine());
-        if (input == -1)
-        {
-          break;
+            }
+            int sum = 0;
+            for(int i =0; i < list.Count; ++i)
+            {
+                sum += list[i];
+            }
+            Console.WriteLine("Sum: {0}", sum);
+            }
         }
-        list.Add(input);
-      
-      
-        sum += input;
-        Console.WriteLine("sum " + sum);
-      }
-              
-    
     }
-  }
-}
->>>>>>> 88973343e8c79c90bd5caa4532095132873f9374

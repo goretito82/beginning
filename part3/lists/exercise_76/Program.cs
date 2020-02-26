9dd3472eda982185200ff90a4e4319dc9b85dc65
titus <titus.gore@centria.fi>
2020-02-26 21:13:41 +0200

    done76

--- part3/lists/exercise_76/Program.cs
+++ part3/lists/exercise_76/Program.cs
@@ -1,71 +1,30 @@
-<<<<<<< HEAD
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-
 namespace exercise_76
 {
-  class Program
-  {
-   public static void Main(string[] args)
+    class Program
     {
-      List<int> numbers = new List<int>();
-      int result = Sum(numbers);
-      Console.WriteLine("sum of all numbers {0}", Convert.ToString(result));
-      
-    }
-     public static int Sum(List<int> numbers)
-     
-      {
-      
-        numbers.Add(3);
-        numbers.Add(2);
-        numbers.Add(6);
-        numbers.Add(-1);
-        numbers.Add(5);
-        numbers.Add(1);
 
-      return numbers.Sum();
-        
+        public static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            int result = Sum(numbers);
+            Console.WriteLine("{0}", Convert.ToString(result));
+        }
+
+
+        public static int Sum(List<int> numbers)
+        {
+
+            numbers.Add(3);
+            numbers.Add(2);
+            numbers.Add(6);
+            numbers.Add(-1);
+            numbers.Add(5);
+            numbers.Add(1);
+            return numbers.Sum();
+        }
+
     }
-  }
 }
-
-  
-
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-namespace exercise_76
-{
-  class Program
-  {
-     
-    public static void Main(string[] args)
-    {
-     List<int> numbers = new List<int>();
-     int result = Sum(numbers);
-     Console.WriteLine("sum of all numbers {0}", Convert.ToString(result));
-    }
-
-
-   public static int Sum(List<int> numbers) 
-    {
-        
-            numbers.Add(23);  
-            numbers.Add(206);  
-            numbers.Add(69);  
-            numbers.Add(-1);  
-            numbers.Add(58); 
-            numbers.Add(177);  
-            return numbers.Sum();  
-    }
- 
-  }
-    }
- 
-
-
-
->>>>>>> 88973343e8c79c90bd5caa4532095132873f9374

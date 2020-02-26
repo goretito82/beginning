23ff36de24250e1efeab419a68c41ac1271de77b
titus <titus.gore@centria.fi>
2020-02-26 20:54:44 +0200

    done75

--- part3/lists/exercise_75/Program.cs
+++ part3/lists/exercise_75/Program.cs
@@ -1,13 +1,19 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Linq;
 namespace exercise_75
 {
   class Program
   {
     public static void Main(string[] args)
     {
+      List<int> numbers = new List<int>();
+          numbers.Add(3);
+          numbers.Add(2);
+          numbers.Add(6);
+          numbers.Add(-1);
+          numbers.Add(5);
+          numbers.Add(1);
 
 
       // Example method calls for testing your method.
@@ -20,43 +26,14 @@ namespace exercise_75
       Console.WriteLine("The numbers in the range [3, 10]");
       PrintNumbersInRange(numbers, 3, 10);
     }
-
-    }
-  }
-}
-
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace exercise_75
-{
-  class Program
-  {
-   public static void PrintNumbersInRange(List<int> numbers, int lowerLimit, int upperLimit)
+    public static void PrintNumbersInRange(List<int> numbers, int lowerLimit, int upperLimit)
 
    {
    foreach (int number in numbers.Where (number => number >= lowerLimit && number <= upperLimit))
     {
     Console.WriteLine(number);
     }
-   }
-            public static void Main(string[] args)
-   {
-                // Example method calls for testing your method.
-          List<int> numbers = new List<int>();
-          numbers.Add(3);
-          numbers.Add(2);
-          numbers.Add(6);
-          numbers.Add(-1);
-          numbers.Add(5);
-          numbers.Add(1);
-
-    Console.WriteLine("The numbers in the range [0, 5]");
-    PrintNumbersInRange(numbers, 0, 5);
-    }
     }
   }
-    
->>>>>>> 88973343e8c79c90bd5caa4532095132873f9374
+}
+

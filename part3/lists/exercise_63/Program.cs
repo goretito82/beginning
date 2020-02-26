533254cde39309efc91c9b3424eacf6bcdce080e
titus <titus.gore@centria.fi>
2020-02-26 09:48:31 +0200

    done62

--- part3/lists/exercise_63/Program.cs
+++ part3/lists/exercise_63/Program.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 
@@ -25,31 +24,3 @@ namespace exercise_63
 
   }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-
-
-namespace exercise_63
-{
-  class Program
-  {
-    public static void Main(string[] args)
-    {
-      List<int> list = new List<int>();
-      while (true)
-      {
-        int input = Convert.ToInt32(Console.ReadLine());
-        if (input == 0)
-        {
-          break;
-        }
-
-        list.Add(input);
-      }
-      Console.WriteLine(list[1+2]);
-    }
-
-  }
-}
->>>>>>> 88973343e8c79c90bd5caa4532095132873f9374

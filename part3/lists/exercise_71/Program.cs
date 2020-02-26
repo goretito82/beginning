6a4f06bcbb5b04752f6961221cc162c37c7e0bd1
titus <titus.gore@centria.fi>
2020-02-26 12:39:37 +0200

    done71

--- part3/lists/exercise_71/Program.cs
+++ part3/lists/exercise_71/Program.cs
@@ -1,35 +1,10 @@
-<<<<<<< HEAD
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace exercise_71
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      List<int> list = new List<int>();
-      while (true)
-      {
-        int input = Convert.ToInt32(Console.ReadLine());
-        if (input == -1)
-        {
-          break;
-        }
-        list.Add(input);
-      }
-
-    }
-  }
-}
-=======
-﻿using System;
-using System.Collections.Generic;
-
-namespace exercise_71
-{
-  class Program
-  {
         public static void Main(string[] args)
         {
             List<int> list = new List<int>();
@@ -42,14 +17,13 @@ namespace exercise_71
                 }
                 list.Add(input);
             }
-            Console.WriteLine("Search for");
+            Console.WriteLine("Search for?");
             int search = Convert.ToInt32(Console.ReadLine());
 
-           
+       
             int index = list.IndexOf(search);
 
             Console.WriteLine("is at index " + index);
         }
-  }
+    }
 }
->>>>>>> 88973343e8c79c90bd5caa4532095132873f9374

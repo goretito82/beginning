30f53e301f8538aa89bd3b4c7dc6dbef22f770ed
titus <titus.gore@centria.fi>
2020-03-04 12:47:00 +0200

    done80

--- part3/arrays/exercise_80/Program.cs
+++ part3/arrays/exercise_80/Program.cs
@@ -1,7 +1,6 @@
-
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Linq;
 namespace exercise_80
 {
   class Program
@@ -9,16 +8,19 @@ namespace exercise_80
     public static void Main(string[] args)
     {
       int[] numbers = { 5, 1, 3, 4, 2 };
+           
       int sum = SumOfNumbersInArray(numbers);
       Console.WriteLine(sum);
 
     }
 
-    public static int SumOfNumbersInArray(int[] numbers) {
-
-      return 0;
+    public static int SumOfNumbersInArray(int[] numbers) 
+    {
+        return numbers.Sum();
+      
     }
 
   }
 }
+
 

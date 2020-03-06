f60581963f7bcfb5263d8432bf47607a8b9011e8
Titus Gore SOFTK20P <titus.gore@centria.fi>
2020-03-06 13:06:47 +0200

    done81

--- part3/arrays/exercise_81/Program.cs
+++ part3/arrays/exercise_81/Program.cs
@@ -19,8 +19,7 @@ namespace exercise_81
       for(int i = 0; i< array.Length-1; i++)
       Console.Write(array[i]+ ", ");
       Console.WriteLine(array[array.Length-1]);
-      
-
+    
     }
   }
 }

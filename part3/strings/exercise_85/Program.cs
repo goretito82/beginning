4451ddc35f333a30ab49943505b5a00677537206
titus <titus.gore@centria.fi>
2020-03-18 10:30:43 +0200

    done85

--- part3/strings/exercise_85/Program.cs
+++ part3/strings/exercise_85/Program.cs
@@ -7,8 +7,25 @@ namespace exercise_85
   {
     public static void Main(string[] args)
     {
+    string username = "alex";
+    string password = "sunshine";
+    string username2 = "emma";
+    string password2= "haskell";
+    Console.WriteLine("Enter username:");
+    string input = Console.ReadLine();
+    Console.WriteLine("Enter password:");
+    string input2 = Console.ReadLine();
 
-
+if ((username == input && password == input2) ||(username2==input && password2==input2)) 
+        {
+             Console.WriteLine("You have successfully logged in!");        
+             }
+            else
+            {
+            Console.WriteLine("Incorrect username or password!");
+            }
+             
+               
     }
   }
 }

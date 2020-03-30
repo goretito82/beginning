52c6055ee003111144eefefd8837e25951f8bb8d
titus <titus.gore@centria.fi>
2020-03-30 13:03:16 +0300

    done86

--- part3/strings/exercise_86/Program.cs
+++ part3/strings/exercise_86/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace exercise_86
 {
   class Program
   {
     public static void Main(string[] args)
-    {
-
+   {  
+            while (true)  
+            {  
+                var str = Console.ReadLine();  
+                if (!string.IsNullOrEmpty(str))  
+                {  
+                    str.Split(' ').ToList().ForEach(x => Console.WriteLine(x));  
+                }  
+                else  
+                {  
+                    break;  
+                }  
+            }  
+            Console.ReadKey();  
 
     }
   }
 }
-
+    
